26caf5cd22af371de26476f8e12a71b94ae9b9bf
Raj Rajeshwar Singh <rrsr1312@gmail.com>
2021-07-01 10:24:52 +0530

    Added css file to the Expense Item component and also updated the index.css

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,12 @@
-function ExpenseItem(){
-    return <h2>Expense Item!</h2>
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div> 15th August 2020</div>
+            <div className="expense-item__description">Independence Day Celebrations</div>
+            <div className="expense-item__price">₹1453.93</div>
+        </div>);
 }
 
 export default ExpenseItem;
