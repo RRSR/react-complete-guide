a257ffbadac9b05354836cc0f1041f6b416c59d9
Raj Rajeshwar Singh <rrsr1312@gmail.com>
2021-07-01 11:06:22 +0530

    Updated the code to receive value from App component to ExpenseItem component using custom attributs and props.
    Signed-off-by: Raj Rajeshwar Singh <rrsr1312@gmail.com>

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,12 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 3, 14);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 2352.91;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">₹{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">₹{props.amount}</div>
             </div>
         </div>);
 }
