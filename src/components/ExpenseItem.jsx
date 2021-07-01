39b6f8cf3b74c51aeaf898de3bb6ed73c603ece7
Raj Rajeshwar Singh <rrsr1312@gmail.com>
2021-07-01 10:30:44 +0530

    Updated the component to use some js variables instead of hardcoded values in the html.
    Signed-off-by: Raj Rajeshwar Singh <rrsr1312@gmail.com>

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 3, 14);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 2352.91;
+
     return (
         <div className="expense-item">
-            <div> 15th August 2020</div>
-            <div className="expense-item__description">Independence Day Celebrations</div>
-            <div className="expense-item__price">₹1453.93</div>
+            <div>{expenseDate.toDateString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">₹{expenseAmount}</div>
+            </div>
         </div>);
 }
 
