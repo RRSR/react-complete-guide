4e29fdf63c33eaef424a2ad31fd714ebb1e68645
Raj Rajeshwar Singh <rrsr1312@gmail.com>
2021-07-08 09:59:05 +0530

    Added an ExpenseFilter to select an year.
    Signed-off-by: Raj Rajeshwar Singh <rrsr1312@gmail.com>

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = props => {
     const expenses = props.expenses;
+    const [filteredYear, setFilteredYear] = useState('2021');
+    const onSelection = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
     return (
-        <Card className="expenses">
-            <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}/>
-            <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}/>
-            <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}/>
-            <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}/>
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter onSelection={onSelection} selected={filteredYear}/>
+                <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}/>
+                <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}/>
+                <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}/>
+                <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}/>
+            </Card>
+        </div>
+
     );
 }
 
