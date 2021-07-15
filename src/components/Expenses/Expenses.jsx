e3048c05a17caa89c978cab97df2c77a6bc27b26
Raj Rajeshwar Singh <rrsr1312@gmail.com>
2021-07-15 09:30:51 +0530

    Render list of data using map instead of one item at a time.
    Signed-off-by: Raj Rajeshwar Singh Rathore <rrsr1312@gmail.com>

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -14,10 +14,9 @@ const Expenses = props => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter onSelection={onSelection} selected={filteredYear}/>
-                <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}/>
-                <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}/>
-                <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}/>
-                <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}/>
+                {expenses.map(expense => <ExpenseItem date={expense.date}
+                                                      title={expense.title}
+                                                      amount={expense.amount}/>)}
             </Card>
         </div>
 
